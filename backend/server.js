86ba6da356d509eeaf41d070721016956a26b5e6
rovenelabanga <rovenelabanga2001@gmail.com>
2025-06-28 10:19:41 +0300

    adds CORS

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
-const jsonServer = require('json-server')
+const jsonServer = require("json-server");
 
-const server = jsonServer.create()
+const server = jsonServer.create();
 
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const cors = require("cors");
 
-server.use(middlewares)
-server.use('/', router)
-server.listen(process.env.PORT || 5000, () =>{
-    console.log("JSON Server is running")
-})
+server.use(cors());
+
+server.use(middlewares);
+server.use("/", router);
+server.listen(process.env.PORT || 5000, () => {
+  console.log("JSON Server is running");
+});
